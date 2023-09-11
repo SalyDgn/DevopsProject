7dc36bc2fb410dd1cf86e78cb66be7a9a827c2c8
famabarham <sfama@ept.sn>
2023-09-11 19:13:13 +0000

    push de fama sarr

---
+++ data.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO User (id,username,email,first_name,last_name)  VALUES 
+(1,'fatkinend','kinefatou@gmail.com','fatou','ndiaye'),
+(2,'sarrfama','famab@gmail.com','fama','sarr')
+(3,'diaguanach','diaguana@gmail.com','salimata','diaguana');
